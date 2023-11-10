b036eb12b1c2bac60b983e2187ed6963da6338b6
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2023-11-10 10:55:58 +0500

    static files

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -38,3 +40,5 @@ urlpatterns = [
     path('', include("api.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
