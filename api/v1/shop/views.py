c3f84afea2088a7d1c653ad55d9fe81d545f77e2
academic7003 <nazarmamatovnurmuhammad7@gmail.com>
2023-11-13 15:08:25 +0500

    added: optimization ORM requests

--- api/v1/shop/views.py
+++ api/v1/shop/views.py
@@ -1,37 +1,21 @@
 from django.db.models import Sum, Count
 from django.shortcuts import get_object_or_404
-from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.v1.shop.serializers import ResponseEmployeesStatisticsSerializer, FilterDateSerializer
+from api.v1.shop.serializers import ResponseEmployeesStatisticsSerializer, FilterDateSerializer, date_params
 from apps.shop.models import *
 
 
-@swagger_auto_schema(tags=['statistics'], method='get', manual_parameters=[
-    openapi.Parameter(
-        name='month',
-        in_=openapi.IN_QUERY,
-        type=openapi.TYPE_INTEGER,
-        description='месяц',
-        required=True
-    ),
-    openapi.Parameter(
-        name='year',
-        in_=openapi.IN_QUERY,
-        type=openapi.TYPE_INTEGER,
-        description='год',
-        required=True
-    )
-])
+@swagger_auto_schema(tags=['statistics'], method='get', manual_parameters=date_params)
 @api_view(['GET'])
 def employee_statistics(request, pk: int):
+    context = {}
     serializer = FilterDateSerializer(data=request.query_params)
     serializer.is_valid(raise_exception=True)
     data = serializer.data
     filter_kwargs = {"date__year": data["year"], "date__month": data["month"]}
-    context = {}
     employee_obj = get_object_or_404(EmployeeModel, pk=pk)
     filtered_orders = employee_obj.orders.filter(**filter_kwargs)
     clients_count = filtered_orders.count()
@@ -42,66 +26,43 @@ def employee_statistics(request, pk: int):
     context["employee_name"] = employee_obj.full_name
     context["clients_count"] = clients_count
     context["unique_client"] = unique_client
-    context["prices_sum"] = prices_sum
-    context["products_count"] = products_count
+    context["prices_sum"] = prices_sum or 0
+    context["products_count"] = products_count or 0
 
     return Response(context)
 
 
-@swagger_auto_schema(tags=['statistics'], method='get', manual_parameters=[
-    openapi.Parameter(
-        name='month',
-        in_=openapi.IN_QUERY,
-        type=openapi.TYPE_INTEGER,
-        description='месяц',
-        required=True
-    ),
-    openapi.Parameter(
-        name='year',
-        in_=openapi.IN_QUERY,
-        type=openapi.TYPE_INTEGER,
-        description='год',
-        required=True
-    )
-], responses={200: ResponseEmployeesStatisticsSerializer(many=True)})
+@swagger_auto_schema(tags=['statistics'], method='get', manual_parameters=date_params,
+                     responses={200: ResponseEmployeesStatisticsSerializer(many=True)})
 @api_view(['GET'])
 def all_employees_stat(request):
     serializer = FilterDateSerializer(data=request.query_params)
     serializer.is_valid(raise_exception=True)
     data = serializer.data
     filter_kwargs = {"date__year": data["year"], "date__month": data["month"]}
-    response = []
-    employees = EmployeeModel.objects.all()
     filtered_orders = OrderModel.objects.filter(**filter_kwargs)
-    for employee in employees:
-        context = {}
-        context["id"] = employee.id
-        context["fullname"] = employee.full_name
-        context["unique_clients"] = filtered_orders.distinct("client").count()
-        context["clients"] = filtered_orders.count()
-        context["products_count"] = filtered_orders.annotate(products_count=Count("products")).aggregate(
-            products_sum=Sum("products_count"))["products_sum"]
-        context["prices_sum"] = filtered_orders.aggregate(order_price_sum=Sum("price"))["order_price_sum"]
-        response.append(context)
+    order_stats = filtered_orders.values("employee__id").annotate(
+        unique_clients=Count("client", distinct=True),
+        clients=Count("client"),
+        products_sum=Sum("products"),
+        prices_sum=Sum("price")
+    ).values("employee__id", "employee__full_name", "unique_clients", "clients", "products_sum", "prices_sum")
+    response = [
+        {
+            "id": stat["employee__id"],
+            "fullname": stat["employee__full_name"],
+            "unique_clients": stat["unique_clients"],
+            "clients": stat["clients"],
+            "products_count": stat["products_sum"] or 0,
+            "prices_sum": stat["prices_sum"] or 0
+        }
+        for stat in order_stats
+    ]
+
     return Response(response)
 
 
-@swagger_auto_schema(tags=['statistics'], method='get', manual_parameters=[
-    openapi.Parameter(
-        name='month',
-        in_=openapi.IN_QUERY,
-        type=openapi.TYPE_INTEGER,
-        description='месяц',
-        required=True
-    ),
-    openapi.Parameter(
-        name='year',
-        in_=openapi.IN_QUERY,
-        type=openapi.TYPE_INTEGER,
-        description='год',
-        required=True
-    )
-])
+@swagger_auto_schema(tags=['statistics'], method='get', manual_parameters=date_params)
 @api_view(['GET'])
 def client_stat(request, pk):
     serializer = FilterDateSerializer(data=request.query_params)
@@ -113,7 +74,7 @@ def client_stat(request, pk):
     filtered_orders = client.orders.filter(**filter_kwargs)
     context["id"] = client.id
     context["fullname"] = client.full_name
-    context["products_count"] = filtered_orders.annotate(products_count=Count("products")).aggregate(
-        products_sum=Sum("products_count")).get("products_sum", 0)
-    context["prices_sum"] = filtered_orders.aggregate(order_price_sum=Sum("price"))["order_price_sum"]
+    context["products_count"] = filtered_orders.aggregate(
+        products_sum=Sum("products__count")).get("products_sum", 0)
+    context["prices_sum"] = filtered_orders.aggregate(order_price_sum=Sum("price")).get("order_price_sum", 0)
     return Response(context)
